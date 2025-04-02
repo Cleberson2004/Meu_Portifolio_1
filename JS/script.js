3e91d700f43d7dab39422abdd501b42d14ad7715
Cleberson2004 <clebersondias2004@gmail.com>
2025-04-01 21:14:37 -0300

    01/04/2025

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,3 @@
-function open_side_bar(){
-    var side_bar = document.getElementById("side_bar_div")
-    if(side_bar.style.display === 'block'){
-        side_bar.style.display = 'none'
-
-    }else{
-        side_bar.style.display = 'block'
-    }
+function refresh(){
+    window.location.reload();
 }
